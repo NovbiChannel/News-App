0e09870b2363fe3135195a67fe6c2495e776beab
NovbiChannel <chaglovnikita@gmail.com>
2022-11-25 21:08:22 +0500

    Lesson 2

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.newsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val mBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+        Handler(Looper.myLooper()!!).postDelayed({
+            setContentView(mBinding.root)
+            mBinding.bottomNavMenu.setupWithNavController(
+                navController = mBinding.navHostFragment.findNavController()
+            )
+        }, 5000)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
