e10302af4bb4749935156d1c070b6a7aeb25db9f
NovbiChannel <chaglovnikita@gmail.com>
2022-11-26 11:48:23 +0500

    Lesson 3

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "5def54d48e004c51aa3514ac4bd4c209"
+    }
+}
