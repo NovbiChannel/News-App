5ab1985ca67d1861774bb58a5694d56280223f70
NovbiChannel <chaglovnikita@gmail.com>
2022-11-28 14:03:31 +0500

    Lesson 6

--- app/src/main/java/com/example/newsapp/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/search/SearchFragment.kt
@@ -1,16 +1,32 @@
 package com.example.newsapp.ui.search
 
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapp.databinding.FragmentSearchBinding
+import com.example.newsapp.ui.adapters.NewsAdapter
+import com.example.newsapp.utils.Resource
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class SearchFragment : Fragment() {
 
     private var _binding: FragmentSearchBinding? = null
     private val mBinding get() = _binding!!
+
+    private val viewModel by viewModels<SearchViewModel>()
+    lateinit var newsAdapter: NewsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,5 +34,51 @@ class SearchFragment : Fragment() {
     ): View? {
         _binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
         return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+
+        var job: Job? = null
+        mBinding.edSearch.addTextChangedListener { text: Editable? ->
+            job?.cancel()
+            job = MainScope().launch {
+                delay(500L)
+                text?.let {
+                    if (it.toString().isNotEmpty()) {
+                        viewModel.getSearchNews(query = it.toString())
+                    }
+                }
+            }
+        }
+        viewModel.searchNewsLiveData.observe(viewLifecycleOwner) { responce ->
+            when (responce) {
+                is Resource.Success -> {
+                    mBinding.pagSearchProgressBar.visibility = View.INVISIBLE
+                    responce.data?.let {
+                        newsAdapter.differ.submitList(it.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    mBinding.pagSearchProgressBar.visibility = View.INVISIBLE
+                    responce.data?.let {
+                        Log.e("checkData", "MainFragment error: ${it}")
+                    }
+                }
+                is Resource.Loading -> {
+                    mBinding.pagSearchProgressBar.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+    private fun initAdapter() {
+        newsAdapter = NewsAdapter()
+        mBinding.searchNewsAdapter.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+            Log.d("MyLog", "init")
+        }
     }
 }
